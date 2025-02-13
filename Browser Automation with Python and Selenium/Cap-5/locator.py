137d2a495d7eb10bdfeae9a4844c9806d4c2fa77
launzoa <75075230+launzoa@users.noreply.github.com>
2025-02-13 17:41:48 -0300

    Delete Browser Automation with Python and Selenium/Cap-5 directory

--- Browser Automation with Python and Selenium/Cap-5/locator.py
+++
@@ -1,5 +0,0 @@
-#Simples estrutura de dados de tupla para armazenar a localização do nosso elemento, com o by: ID, Name, Xpath, CSSelector... Além do valor, button#b1 por exemplo.
-
-from collections import namedtuple
-
-Locator = namedtuple('Locator', ['by', 'value'])
