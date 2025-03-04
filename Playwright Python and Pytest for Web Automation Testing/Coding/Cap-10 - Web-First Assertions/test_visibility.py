1e30a0803ae73c7a943bdbeac8fc21d749693c9d
launzoa <75075230+launzoa@users.noreply.github.com>
2025-03-04 20:00:15 -0300

    Update test_visibility.py

--- Playwright Python and Pytest for Web Automation Testing/Coding/Cap-10 - Web-First Assertions/test_visibility.py
+++ Playwright Python and Pytest for Web Automation Testing/Coding/Cap-10 - Web-First Assertions/test_visibility.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page, expect
-import pytest
+import pytest, re
 
 
 @pytest.fixture(autouse=True)
@@ -14,8 +14,8 @@ def test_visi(page: Page):
 
     remo = page.get_by_role("button", name="Removed")
     wid = page.get_by_role("button", name="Zero Width")
-    over = page.get_by_role("button", name="Overlapped")
-    opa = page.get_by_role("button", name="Opacity")
+    over = page.get_by_role("button", name="Overlapped").locator("..")
+    opa = page.get_by_role("button", name="Opacity 0")
     hid = page.get_by_role("button", name="Visibility Hidden")
     disp = page.get_by_role("button", name="Display None")
     off = page.get_by_role("button", name="Offscreen")
@@ -23,6 +23,16 @@ def test_visi(page: Page):
 
     hide = page.get_by_role("button", name="Hide")
     hide.click()
-
     
-    page.pause()
+    expect(remo).to_be_hidden()
+    expect(wid).to_have_class(re.compile(r"zerowidth"))
+    expect(over.locator("div")).to_have_id("hidingLayer")
+    expect(opa).to_have_css("opacity", "0")
+    expect(hid).to_be_hidden()
+    expect(disp).to_be_hidden()    
+    expect(off).not_to_be_in_viewport()
+
+'''
+Esse exercício se consiste em testar algumas propriedades que deixam
+determinados elementos, invísiveis ao seu jeito.
+'''
